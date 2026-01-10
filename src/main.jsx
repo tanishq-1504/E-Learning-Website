2d40cbe7e4f72d6dd44e041b0bf6df3f2ade4a84
tanishq-1504 <tanishq1504@gmail.com>
2026-01-10 13:10:46 +0530

    commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import LogInPage from "./pages/LogInPage";
@@ -9,13 +8,14 @@ import PublicRoute from "./routes/PublicRoute";
 import PrivateRoute from "./routes/PrivateRoute";
 import MyDashboard from "./pages/my-dashboard/MyDashboard";
 import AdminRoute from "./routes/AdminRoute";
-import UserProfile from "./pages/UserProfile";
 import TrainersPage from "./pages/TrainersPage";
+import UserProfile from "./pages/UserProfile";
+import LandingPage from "./1-landing-page/LandingPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <LandingPage/>,
   },
   {
     path: "/login",
